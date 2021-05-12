55a35a3be6ffbbc1ab45f387c4e2f18f46b8eeb1
dongovor <govor86@gmail.com>
2021-05-12 11:35:58 +0300

    init

---
+++ order_robots.py
@@ -0,0 +1,11 @@
+from RPA.Browser.Selenium import Selenium
+from RPA.Browser import Browser
+import time, re, os, datetime
+
+browser = Selenium()
+#init path to save and check if it not exists->create
+path_to_save = os.path.join(os.path.dirname(os.path.realpath(__file__), 'output'))
+if not os.path.exists(path_to_save):
+    os.makedirs(path_to_save)
+
+browser.set_download_directory(path_to_save)
---
+++ task.py
@@ -0,0 +1,9 @@
+import order_robots
+
+def minimal_task():
+    
+    print("Done.")
+
+
+if __name__ == "__main__":
+    minimal_task()
