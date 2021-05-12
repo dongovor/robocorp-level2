702cb9d4782b45f5e7134a499120a45a37ec0be6
dongovor <govor86@gmail.com>
2021-05-12 13:53:34 +0300

    action for one robot added

--- order_robots.py
+++ order_robots.py
@@ -1,11 +1,54 @@
 from RPA.Browser.Selenium import Selenium
 from RPA.Browser import Browser
+from RPA.Robocloud import Items
 import time, re, os, datetime
 
 browser = Selenium()
-#init path to save and check if it not exists->create
-path_to_save = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output')
-if not os.path.exists(path_to_save):
-    os.makedirs(path_to_save)
 
-browser.set_download_directory(path_to_save)
+def config_browser(path_to_save):
+    browser.set_download_directory(path_to_save)
+
+#open browser
+def open_browser(url: str):
+    browser.open_available_browser(url)
+    time.sleep(2)
+
+#close popup
+def close_popup():
+    try:
+        yep_btn = browser.find_element('//*[@id="root"]/div/div[2]/div/div/div/div/div/button[1]')
+        yep_btn.click()
+    except:
+        pass
+
+#choose head
+def choose_head(head_model):
+    head_model_el = browser.find_element(f'//*[@id="head"]/option[{head_model}]')
+    head_model_el.click()
+    
+#choose body
+def choose_body(body_model):
+    body_model_el = browser.find_element(f'//*[@id="id-body-{body_model}"]')
+    body_model_el.click()
+
+#choose_legs
+def choose_legs(legs_model):
+    #legs_model_text = extract_table(legs_model)
+    legs_model_input = browser.driver.find_element_by_xpath("//input[@placeholder='Enter the part number for the legs']")
+    browser.input_text(legs_model_input, legs_model)
+
+#set address
+def set_shipping_address(shipping_address):
+    browser.input_text('//*[@id="address"]', shipping_address)
+
+#show table
+def show_table():
+    show_table_btn = browser.find_element('//*[text() = "Show model info"]')
+    show_table_btn.click()
+
+#extract table
+def extract_table(legs_model):
+    show_table()
+    table_el = browser.find_element(f'//*[@id="model-info"]/tbody/tr[{legs_model}]')
+    legs_model_text = (table_el.text)[:-1]
+    return legs_model
--- task.py
+++ task.py
@@ -1,7 +1,23 @@
-import order_robots
+from order_robots import *
+from utils import *
+
+
+download_link = 'https://robotsparebinindustries.com/orders.csv'
+url = 'https://robotsparebinindustries.com/#/robot-order'
 
 def minimal_task():
-    
+    open_browser(url)
+    close_popup()
+    # choose_head(2)
+    # choose_body(4)
+    #set_shipping_address('test shipping addr')
+    #extract_table(2)
+    choose_legs(6)
+
+    # path_to_save = init_output()
+    # input_file = download_orders_csv(download_link, path_to_save)
+    # input_list = read_input(input_file)
+    #print(input_list)
     print("Done.")
 
 
